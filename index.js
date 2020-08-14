af5b433b509d12f9e515ed92f66fbb54626bda35
Akndju <davidd.akindoju@gmail.com>
2020-08-14 14:37:45 +0100

    Fixes

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const password = process.env.twitter_password;
 (async () => {
   const browser = await puppeteer.launch({
     headless: false,
-    args: ['--no-sandbox'],
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
   });
 
   const page = await browser.newPage();
