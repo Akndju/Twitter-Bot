fba49a536c4b4182273e080113393061aad9c3a8
Akndju <davidd.akindoju@gmail.com>
2020-08-14 14:25:33 +0100

    fixed dependencies

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const password = process.env.twitter_password;
 (async () => {
   const browser = await puppeteer.launch({
     headless: false,
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    args: ['--no-sandbox'],
   });
 
   const page = await browser.newPage();
